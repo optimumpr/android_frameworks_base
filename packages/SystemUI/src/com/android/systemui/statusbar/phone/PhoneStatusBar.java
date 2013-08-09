3d97ccccffb2be3ea65fd5f55e6f35f7384e6f21
0xD34D <clark@scheffsblend.com>
2013-08-08 19:51:30 -0700

    Frameworks: Battery bar (1/2)
    ported from Chameleon OS
    
    Change-Id: I10731a08b0e252ccc1ad1b314d4d38c1cceb02dd

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -4056,6 +4056,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 break;
 
             case 4:
+            case 7:
                 mode = BatteryMeterMode.BATTERY_METER_GONE;
                 break;
 
