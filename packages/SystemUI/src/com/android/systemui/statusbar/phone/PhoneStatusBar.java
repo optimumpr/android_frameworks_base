192552fbcee86af0793971099997b94e8edb985a
david <davidteri91@gmail.com>
2014-10-09 21:44:09 +0200

    Revert "Frameworks: Battery bar (1/2)"
    This reverts commit c5ad720674474b401bcacd04867730742acde766.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -4118,7 +4118,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                 break;
 
             case 4:
-            case 7:
                 mode = BatteryMeterMode.BATTERY_METER_GONE;
                 break;
 
